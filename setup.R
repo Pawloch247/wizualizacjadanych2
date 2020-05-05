568591a6516b202b1f1b7e8b75f50eb8693adf99
Ostrzyciel nożyczek <ostrzycielnozyczek@gmail.com>
2020-05-05 13:05:37 +0200

    More work on plot template

--- setup.R
+++ setup.R
@@ -1,9 +1,11 @@
 # skrypt instalujący wszystkie potrzebne liby
 
 # żeby ta ostatnia liba przeszła będą potrzebne pakiety dodatkowe (przynajmniej na Debianie/Ubuntu):
-# sudo apt update && sudo apt install libgdal-dev libxml2-dev libudunits2-dev libv8-dev
+# sudo apt update && sudo apt install libgdal-dev libxml2-dev libudunits2-dev libv8-dev libfontconfig1-dev libcairo2-dev
 
 install.packages("readxl") # czytanie XLSX
 install.packages("remotes")
 remotes::install_github("statisticspoland/R_Package_to_API_BDL") # API do GUS
 # to przy okazji instaluje dplyr, ggplot, shiny i wszystkich świętych
+
+install.packages("hrbrthemes") # ładne motywy do wykresów
