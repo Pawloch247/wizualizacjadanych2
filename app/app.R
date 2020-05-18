38914fb2c7de4db285a7b4cab9a006548b9351be
igro <igor.tomasz.marczak@gmail.com>
2020-05-18 13:10:20 +0200

    adding choropleth

--- app/app.R
+++ app/app.R
@@ -24,12 +24,27 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
+
+  
   regions <- reactive ({
     if (input$region_type == "Województwa")
       readOGR("maps/wojewodztwa.shp")
     else
       readOGR("maps/Powiaty.shp")
   })
+  
+  myPalette <- reactive({
+    colorNumeric("YlOrRd", domain = regions()$No_Buss)
+  })
+  
+  labels <- reactive({
+    sprintf(
+      "<strong>%s</strong><br/>%g  registered enterprises",
+      regions()$Name, regions()$No_Buss) %>%
+      lapply(htmltools::HTML)
+  })
+  
+  
   
   wojewodztwa <- reactive ({
     if (input$region_type == "Województwa")
@@ -38,15 +53,26 @@ server <- function(input, output) {
       FALSE
   })
   
+  print(labels)
+  
   output$map <- renderLeaflet({
     leaflet(regions()) %>%
-      addTiles() %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
       addPolygons(layerId = regions()$JPT_KOD_JE,
-                  highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                      bringToFront = TRUE))
+                  fillColor = ~myPalette()(regions()$No_Buss),
+                  fillOpacity = 0.3,
+                  weight = 1,
+                  color = "grey",
+                  dashArray = "3",
+                  highlightOptions = highlightOptions(color = "#666", 
+                                                      weight = 2,
+                                                      bringToFront = TRUE,
+                                                      fillOpacity = "0.3"),
+      label = labels()) %>%
+      addLegend(pal = myPalette(), values = ~regions()$No_Buss, opacity = 0.7, title = "No. of enterprises",
+                position = "bottomright")
   })
-  
-  
+ 
   observeEvent(input$map_shape_click,{
     event <- input$map_shape_click
 
