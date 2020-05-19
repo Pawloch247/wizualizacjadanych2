922ace68a01adcd2a9c28b261114b991b06eaecc
Maciej Pawłowski <mac.pawlowski19@gmail.com>
2020-05-19 18:58:12 +0200

    reactEvent rebuild

--- app/app.R
+++ app/app.R
@@ -234,15 +234,13 @@ server <- function(input, output) {
   
   wojewodztwa <- reactive ({
     if (input$region_type == "Województwa")
-      "woj"
-    else if (input$region_type == "Kraj")
-      "kraj"
+      TRUE
     else
-      "pow"
+      FALSE
   })
   
   regions <- reactive({
-    if (wojewodztwa() == "woj")
+    if (wojewodztwa())
       readOGR("maps/wojewodztwa.shp", use_iconv = TRUE, encoding = "UTF-8")
     else 
       readOGR("maps/Powiaty.shp", use_iconv = TRUE, encoding = "UTF-8")
@@ -286,49 +284,114 @@ server <- function(input, output) {
         options = searchFeaturesOptions(zoom=7, openPopup=TRUE, autoType=FALSE))
   })
  
-
-  observeEvent(input$map_shape_click,{
+  
+  regionTMP = eventReactive(input$map_shape_click,{
     event <- input$map_shape_click
+
+    return(event$id)},ignoreNULL = F)
+  
+  regionID = reactive({
+
+    if (wojewodztwa() & (nchar(regionTMP()) == 4 || is.null(regionTMP())))
+      return(c("14"))
     
-    if (wojewodztwa() == "woj"){
+    if (!wojewodztwa() & (nchar(regionTMP()) == 2 || is.null(regionTMP())))
+      return(c("1465"))
+    
+    return(regionTMP())
+  })
+  
+  df = reactive({
+
+    if (wojewodztwa()){
       df <- data %>%
-        filter(substr(AdressTERC, 1, 2) == event$id) %>%
+        filter(substr(AdressTERC, 1, 2) == regionID()) %>%
         rename(Region = AdressVoivodeship) %>%
         select(DurationOfExistenceInMonths, Region, PKDMainSection, Sex, IsWWW, IsPhoneNo, IsEmail, MonthOfStartingOfTheBusiness)
-      
+      print(regionID())
       df_pkd_percent <- woj_percent %>%
-        filter(AdressTERC == event$id) %>%
+        filter(AdressTERC == regionID()) %>%
         select(PKDMainSection, size)
-      
+
       df_pkd_count <- woj_count %>%
-        filter(AdressTERC == event$id) %>%
+        filter(AdressTERC == regionID()) %>%
         select(PKDMainSection, size)
-      
+
       region_name = df$Region[1]
-      
+
     } else {
       df <- data %>%
-        filter(substr(AdressTERC, 1, 4) == event$id) %>%
+        filter(substr(AdressTERC, 1, 4) == regionID()) %>%
         rename(Region = AdressCounty) %>%
         select(DurationOfExistenceInMonths, Region, PKDMainSection, Sex, IsWWW, IsPhoneNo, IsEmail, MonthOfStartingOfTheBusiness)
-      
+
       df_pkd_percent <- pow_percent %>%
-        filter(AdressTERC == event$id) %>%
+        filter(AdressTERC == regionID()) %>%
         select(PKDMainSection, size)
-      
+
       df_pkd_count <- pow_count %>%
-        filter(AdressTERC == event$id) %>%
+        filter(AdressTERC == regionID()) %>%
         select(PKDMainSection, size)
-      
+
       region_name = df$Region[1]
-      
+
     }
 
+    return(list(df = df, df_pkd_percent = df_pkd_percent,
+                df_pkd_count = df_pkd_count, region_name = region_name))
+    
+  })
+  
+  # observeEvent(input$map_shape_click,{
+  #   event <- input$map_shape_click
+  #   
+  #   if (wojewodztwa() == "woj"){
+  #     df <- data %>%
+  #       filter(substr(AdressTERC, 1, 2) == event$id) %>%
+  #       rename(Region = AdressVoivodeship) %>%
+  #       select(DurationOfExistenceInMonths, Region, PKDMainSection, Sex, IsWWW, IsPhoneNo, IsEmail, MonthOfStartingOfTheBusiness)
+  #     
+  #     df_pkd_percent <- woj_percent %>%
+  #       filter(AdressTERC == event$id) %>%
+  #       select(PKDMainSection, size)
+  #     
+  #     df_pkd_count <- woj_count %>%
+  #       filter(AdressTERC == event$id) %>%
+  #       select(PKDMainSection, size)
+  #     
+  #     region_name = df$Region[1]
+  #     
+  #   } else {
+  #     df <- data %>%
+  #       filter(substr(AdressTERC, 1, 4) == event$id) %>%
+  #       rename(Region = AdressCounty) %>%
+  #       select(DurationOfExistenceInMonths, Region, PKDMainSection, Sex, IsWWW, IsPhoneNo, IsEmail, MonthOfStartingOfTheBusiness)
+  #     
+  #     df_pkd_percent <- pow_percent %>%
+  #       filter(AdressTERC == event$id) %>%
+  #       select(PKDMainSection, size)
+  #     
+  #     df_pkd_count <- pow_count %>%
+  #       filter(AdressTERC == event$id) %>%
+  #       select(PKDMainSection, size)
+  #     
+  #     region_name = df$Region[1]
+  #     
+  #   }
+  # 
     ### START PLOTS
+  
+  
+  theme_title = reactive({
     theme_title <- theme(
       plot.title = element_text(hjust = 0.5, face = "bold", size = (15)),
     )
-   
+    
+    return(theme_title)
+  })
+  
+  theme_basic = reactive({
+    
     theme_basic <- theme(
       plot.title = element_text(hjust = 0.5, face = "bold", size = (15)),
       axis.title = element_text(size = (12)),
@@ -341,21 +404,29 @@ server <- function(input, output) {
       legend.text = element_text(size = 8.5)
     )
     
-    p <- ggplot(df, aes(x=DurationOfExistenceInMonths)) +
+    return(theme_basic)
+  })
+  
+  p = reactive({
+    
+    
+    p <- ggplot(df()$df, aes(x=DurationOfExistenceInMonths)) +
       geom_histogram(color="#39cccc", fill="#39cccc") +
       labs(
         title = "Histogram czasu trwania działalności",
         x = "Czas trwania (w miesiącach)",
         y = "Liczba"
-      ) + 
-      theme_basic
-    output$plot <- renderPlot(p)
-   
- 
-    p1 <- df %>%
+      ) + theme_basic()
+    return(p)
+  })
+  
+  output$plot <- renderPlot(p())
+  
+  p1 = reactive({
+    p1 <- df()$df %>%
       group_by(PKDMainSection) %>%
-      tally() %>% 
-      mutate(n = 100 * (n / nrow(df))) %>%
+      tally() %>%
+      mutate(n = 100 * (n / nrow(df()$df))) %>%
       
       ggplot(aes(x=PKDMainSection, y=n, fill=getGroupText(PKDMainSection))) +
       geom_bar(stat="identity", width=1, color="white") +
@@ -365,84 +436,90 @@ server <- function(input, output) {
         title = "Udział poszczególnych rodzajów działalności gospodarczej",
         x = "Kod PKD",
         y = "% udziału danej kategorii"
-      ) + 
-      theme_basic + 
+      ) +
+      theme_basic() +
       guides(fill=guide_legend(
         nrow=11,
         byrow=TRUE))
+    return(p1)
+  })
 
-    output$plot1 <- renderPlot(p1)
-    
-    p2_1 <- df %>%
-      group_by(Sex) %>% tally() %>% mutate(n =  (n / nrow(df))) %>%
+  output$plot1 <- renderPlot(p1())
+
+  p2 = reactive({
+    p2_1 <- df()$df %>%
+      group_by(Sex) %>% tally() %>% mutate(n =  (n / nrow(df()$df))) %>%
       mutate(Sex = recode(Sex, 'F' = "Kobieta", 'M' = "Mężczyzna")) %>%
       ggplot(aes(x="", y=n, fill=Sex)) +
       geom_col() +
       geom_text(aes(label = percent(n)), position = position_stack(vjust = 0.5), color = "white", size=6, fontface='bold') +
-      coord_polar("y", start=0) + 
+      coord_polar("y", start=0) +
       theme_void() +
-      theme_title +
+      theme_title() +
       theme(legend.title=element_blank()) +
       labs(
         title = "Płeć"
-      ) + 
+      ) +
       scale_fill_manual(values = c("Mężczyzna" = "#39cccc",
                                    "Kobieta" = "#cc3939"))
     
-    p2_2 <- df %>%
-      group_by(IsWWW) %>% tally() %>% mutate(n =  (n / nrow(df))) %>%
+    p2_2 <- df()$df %>%
+      group_by(IsWWW) %>% tally() %>% mutate(n =  (n / nrow(df()$df))) %>%
       mutate(IsWWW = c('Posiada', 'Nie posiada')[IsWWW + 1]) %>%
       ggplot(aes(x="", y=n, fill=IsWWW)) +
       geom_col() +
       geom_text(aes(label = percent(n)), position = position_stack(vjust = 0.5), color = "white", size=6, fontface='bold') +
-      coord_polar("y", start=0) + 
+      coord_polar("y", start=0) +
       theme_void() +
-      theme_title +
+      theme_title() +
       theme(legend.title=element_blank()) +
       labs(
         title = "Strona WWW (podana w rejestracji)"
-      ) + 
+      ) +
       scale_fill_manual(values = c("Posiada" = "#39cccc",
                                    "Nie posiada" = "#cc3939"))
     
-    p2_3 <- df %>%
-      group_by(IsEmail) %>% tally() %>% mutate(n =  (n / nrow(df))) %>%
+    p2_3 <- df()$df %>%
+      group_by(IsEmail) %>% tally() %>% mutate(n =  (n / nrow(df()$df))) %>%
       mutate(IsEmail = c('Posiada', 'Nie posiada')[IsEmail + 1]) %>%
       ggplot(aes(x="", y=n, fill=IsEmail)) +
       geom_col() +
       geom_text(aes(label = percent(n)), position = position_stack(vjust = 0.5), color = "white", size=6, fontface='bold') +
-      coord_polar("y", start=0) + 
+      coord_polar("y", start=0) +
       scale_fill_manual(values = c("Posiada" = "#39cccc",
                                    "Nie posiada" = "#cc3939")) +
       theme_void() +
-      theme_title +
+      theme_title() +
       theme(legend.title=element_blank()) +
       labs(
         title = "Adres Email (podany w rejestracji)"
       )
     
-    p2_4 <- df %>%
-      group_by(IsPhoneNo) %>% tally() %>% mutate(n =  (n / nrow(df))) %>%
+    p2_4 <- df()$df %>%
+      group_by(IsPhoneNo) %>% tally() %>% mutate(n =  (n / nrow(df()$df))) %>%
       mutate(IsPhoneNo = c('Posiada', 'Nie posiada')[IsPhoneNo + 1]) %>%
       ggplot(aes(x="", y=n, fill=IsPhoneNo)) +
       geom_col() +
       geom_text(aes(label = percent(n)), position = position_stack(vjust = 0.5), color = "white", size=6, fontface='bold') +
-      coord_polar("y", start=0) + 
+      coord_polar("y", start=0) +
       theme_void() +
-      theme_title +
+      theme_title() +
       theme(legend.title=element_blank()) +
       labs(
         title = "Numer telefonu (podany w rejestracji)"
-      ) + 
+      ) +
       scale_fill_manual(values = c("Posiada" = "#39cccc",
                                    "Nie posiada" = "#cc3939"))
     
     p2 <- cowplot::plot_grid(p2_1, p2_2, p2_3, p2_4, labels = "")
     
-    output$plot2 <- renderPlot(p2)
-    
-    
-    p3 <- df %>%
+    return(p2)
+  })  
+
+  output$plot2 <- renderPlot(p2())
+
+  p3 = reactive({
+    p3 <- df()$df %>%
       group_by(MonthOfStartingOfTheBusiness, PKDMainSection) %>% tally() %>%
       rowwise() %>%
       mutate(PKDMainSection = getGroupText(PKDMainSection)) %>%
@@ -452,34 +529,38 @@ server <- function(input, output) {
         title = "Udział poszczególnych rodzajów działalności gospodarczej w odniesieniu do miesiąca rozpoczęcia",
         x = "Miesiąc rozpoczęcia działalności",
         y = "Udziału danej kategorii"
-      ) + 
-      theme_basic + 
+      ) +
+      theme_basic() +
       guides(fill=guide_legend(
         nrow=11,
         byrow=TRUE))
+    return(p3)
+  })
     
-    output$plot3 <- renderPlot(p3)
-    
-    
-    p4 <- df_pkd_percent %>%
+  output$plot3 <- renderPlot(p3())
+
+  p4 = reactive({
+    p4 <- df()$df_pkd_percent %>%
       group_by(PKDMainSection) %>%
       ggplot(aes(x=PKDMainSection, y=size, fill=getGroupText(PKDMainSection))) +
       geom_bar(stat="identity", width=1, color="white") +
-      geom_text(aes(label=size), position=position_dodge(width=0.9), vjust=-0.25) + 
+      geom_text(aes(label=size), position=position_dodge(width=0.9), vjust=-0.25) +
       labs(
         title = "Ranking według procentowego udziału poszczególnych rodzajów działalności gospodarczej",
         x = "Kod PKD",
         y = "Miejsce w porównaniu do innych jednostek"
-      ) + 
-      theme_basic + 
+      ) +
+      theme_basic() +
       guides(fill=guide_legend(
         nrow=11,
         byrow=TRUE))
+    return(p4)
+  })
     
-    output$plot4 <- renderPlot(p4)
-    
-    
-    p5 <- df_pkd_count %>%
+  output$plot4 <- renderPlot(p4())
+
+  p5 = reactive({
+    p5 <- df()$df_pkd_count %>%
       group_by(PKDMainSection) %>%
       ggplot(aes(x=PKDMainSection, y=size, fill=getGroupText(PKDMainSection))) +
       geom_bar(stat="identity", width=1, color="white") +
@@ -488,72 +569,94 @@ server <- function(input, output) {
         title = "Ranking według sumarycznego udziału poszczególnych rodzajów działalności gospodarczej",
         x = "Kod PKD",
         y = "Miejsce w porównaniu do innych jednostek"
-      ) + 
-      theme_basic + 
+      ) +
+      theme_basic() +
       guides(fill=guide_legend(
         nrow=11,
         byrow=TRUE))
-    
-    output$plot5 <- renderPlot(p5)
-    
-    
-    
+    return(p5)
+  })
+
+  
+  output$plot5 <- renderPlot(p5())
+
+
+
     ### END PLOTS
+
+
+    output$regionName <- renderText(df()$region_name)
+
     
-    
-    output$regionName <- renderText(region_name)
-    
-    regs = regions()
-    index_region = which(regs@data[["JPT_KOD_JE"]] == event$id)
-    
-    output$statistics <- renderLeaflet({
-      leaflet(regs@polygons[[index_region]]@Polygons[[1]]@coords,
-              options = leafletOptions(
-                zoomControl = FALSE,
-                attributionControl=FALSE)) %>% 
-        addPolygons()
+    index_region = reactive({
+      return(which(regions()@data[["JPT_KOD_JE"]] == regionID()))
     })
     
-    # output$statistics_people <- renderUI({
-    #   str1 = "Liczba ludności:"
-    #   str2 = "tu liczba"
-    #   HTML(paste(str1, str2, sep = '<br/>'))
-    #   })
-    # output$statistics_area <- renderUI({
-    #   str1 = "Powierzchnia:"
-    #   str2 = "tu liczba"
-    #   HTML(paste(str1, str2, sep = '<br/>'))
-    # })
-    # output$statistics_companies <- renderUI({
-    #   str1 = "Liczba działalności:"
-    #   str2 = nrow(df)
-    #   HTML(paste(str1, str2, sep = '<br/>'))
-    # })
-    
-  })
-  
 
+    output$statistics <- renderLeaflet({
+      leaflet(regions()@polygons[[index_region()]]@Polygons[[1]]@coords,
+              options = leafletOptions(
+                zoomControl = FALSE,
+                attributionControl=FALSE)) %>%
+        addTiles() %>% 
+        addPolygons()
+    })
+  # #   
+  #   # output$statistics_people <- renderUI({
+  #   #   str1 = "Liczba ludności:"
+  #   #   str2 = "tu liczba"
+  #   #   HTML(paste(str1, str2, sep = '<br/>'))
+  #   #   })
+  #   # output$statistics_area <- renderUI({
+  #   #   str1 = "Powierzchnia:"
+  #   #   str2 = "tu liczba"
+  #   #   HTML(paste(str1, str2, sep = '<br/>'))
+  #   # })
+  #   # output$statistics_companies <- renderUI({
+  #   #   str1 = "Liczba działalności:"
+  #   #   str2 = nrow(df)
+  #   #   HTML(paste(str1, str2, sep = '<br/>'))
+  #   # })
+  #   
+  # })
+  
+  data_people = data.table::fread("data/dane_ludnosc.csv",
+                                  encoding = "UTF-8",
+                                  data.table = F,
+                                  colClasses = c("character"))
+  
   
   output$progressBox1 <- renderInfoBox({
     infoBox(
-      subtitle = "Liczba ludności", "tu jakaś",
+      "Liczba ludności",
+      value = data_people[data_people$AdressTERC == regionID(),]$people,
       width = NULL,
       icon = icon("male"),
       fill = FALSE#male, globe, mail-bulk, balance-scale, dollar-sign, lightbulb
     )
   })
-
+  
   output$progressBox2 <- renderInfoBox({
+   
     infoBox(
-      subtitle = "Powierzchnia", "tu jakaś",
+      "Powierzchnia",
+      value = paste(data_people[data_people$AdressTERC == regionID(),]$area, "km2",sep=""),
       width = NULL,
       icon = icon("globe") #male, globe, mail-bulk, balance-scale, dollar-sign, lightbulb
     )
   })
-
+  
+  # tmp = reactive({
+  #   # df1 = ~df()
+  #   # print(df1$df_)
+  #   # print(nrow(df1))
+  #   # return(nrow(df1$df_))
+  # })
+  
   output$progressBox3 <- renderInfoBox({
+
     infoBox(
-      subtitle = "Liczba działalności", "co",
+      title = "Liczba działalności", value = nrow(df()$df),
       width = NULL,
       icon = icon("user-tie") #male, globe, mail-bulk, balance-scale, dollar-sign, lightbulb
     )
